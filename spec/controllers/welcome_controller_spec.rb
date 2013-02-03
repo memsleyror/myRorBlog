d57d49702be5539c1316bf5a38ad2067f6266479
Mike Emsley <mike.emsley@gmail.com>
2013-02-03 08:50:45 -0600

    initial views and controllers

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe WelcomeController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'legal'" do
+    it "returns http success" do
+      get 'legal'
+      response.should be_success
+    end
+  end
+
+end
